05fc1c093fe089f07049f9b60eaca68f134a9e31
m-alam <m.m.alam6bs@gmail.com>
2021-04-14 14:39:47 +0600

    create project

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcjzWJ2rRQ0GlVvGHYCax4PWvdNXwtjV4",
+  authDomain: "burj-al-arab-41791.firebaseapp.com",
+  projectId: "burj-al-arab-41791",
+  storageBucket: "burj-al-arab-41791.appspot.com",
+  messagingSenderId: "312220663612",
+  appId: "1:312220663612:web:e69d7515ce29253b992839"
+};
+  export default firebaseConfig;
